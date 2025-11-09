a5ed0c30b3bbecedf7a9ee44b9021e44f5e35689
Aravind Sant <aravindsant563@gmail.com>
2025-11-09 14:04:23 +0530

    About section add in nav bar!!!

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -2,9 +2,28 @@ import React from 'react'
 
 function About() {
   return (
-    <div className='text-2xl items-start justify-center '>
-      About us ...
+    <>
+   
+<div className="max-w-7xl mx-auto  mt-4 ">
+    {/* About Section */}
+    <div className="md:col-span-2 bg-white shadow-lg rounded-2xl p-3">
+      <h1 className="text-3xl font-bold text-gray-900 mb-4 text-center">About Us</h1>
+      <p className="text-gray-700 font-semibold leading-relaxed text-[22px]">
+        <span className="text-2xl text-red-900">Bharat Technical College  </span> of Fire & Safety Management, Robertsganj, Sonbhadra (U.P.) is recognized as one of the <span className="text-2xl text-red-900"> No.1  </span>colleges in the region for<span className="text-2xl text-red-900">Fire & Safety Management and Computer Courses </span> . We are committed to providing quality education, practical training, and career-oriented programs that prepare students to become skilled professionals.
+
+      </p>
+      <p className="mt-4 text-gray-700 font-semibold leading-relaxed text-[22px]">
+        The institution is operated by  <span  className="text-2xl text-red-900" >Chameli Devi Sonanchal Sewa Sansthan</span>, registered under the Societies Registration Act, No. 21 of 1860, with Registration Number SON/01794/2025-2026. It is also ISO Certified, ensuring the highest standards of education and training.
+
+      </p>
+       <p className="mt-4 text-gray-900 font-semibold leading-relaxed text-2xl">
+        📍 Address: Pusauli, Robertsganj, Sonbhadra, Uttar Pradesh-231216
+
+       </p>
+    </div> 
+
     </div>
+    </>
   )
 }
 
--- src/component/Syllabus.jsx
+++ src/component/Syllabus.jsx
@@ -161,11 +161,6 @@ const Syllabus= () => {
   <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
     {/* Header */}
     <div className="text-center mb-16">
-      {/* <div className="inline-flex items-center justify-center w-16 h-16 bg-gradient-to-r from-blue-600 to-purple-600 rounded-full mb-6">
-         <svg className="w-8 h-8 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.746 0 3.332.477 4.5 1.253v13C19.832 18.477 18.246 18 16.5 18c-1.746 0-3.332.477-4.5 1.253" />
-        </svg> 
-      </div> */}
       <h1 className="text-5xl font-bold bg-gradient-to-r from-gray-800 to-gray-600 bg-clip-text text-transparent mb-6">
         Courses Syllabus
       </h1>
